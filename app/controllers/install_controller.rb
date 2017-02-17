37bb1e3bcdabf0ca5b8c6881b0da7ebb9454b432
chenqin <41327518@qq.com>
2017-02-17 15:10:29 +0800

    install

--- app/controllers/install_controller.rb
+++ app/controllers/install_controller.rb
@@ -1,14 +1,25 @@
 class InstallController < ApplicationController
   def show
     @agent_ranks = AgentRank.all
-    if @agent_ranks && @agent_ranks.size > 0
-      #redirect_to root_path, alert: "系统已经初始化" and return
+    @admin = User.find_by(id: 1)
+
+    if @agent_ranks && @agent_ranks.size > 0 && @admin
+      redirect_to root_path, alert: "系统已经初始化，不用重复操作" and return
     end
 
     @user = User.new
   end
 
   def init
+    # 校验
+    if params[:user][:email].empty?
+      redirect_to install_path, alert: "email 不能为空" and return
+    end
+
+    if params[:user][:password] != params[:user][:password_confirmation]
+      redirect_to install_path, alert: "确认密码不一致" and return
+    end
+
     # 创建默认用户等级
     @agent_ranks = AgentRank.all
     if !@agent_ranks || @agent_ranks.size == 0
@@ -20,7 +31,12 @@ class InstallController < ApplicationController
     end
 
     # 创建管理员
-
-    redirect_to root_path, notice: "初始化成功！"
+    @admin = User.find_by(id: 1)
+    if !@admin
+      User.create!(:email => params[:user][:email], :password => params[:user][:password])
+      redirect_to root_path, notice: "初始化成功！"
+    else
+      redirect_to root_path, alert: "已经初始化，不用重复操作"
+    end
   end
 end
